ad4d86f545402ba182867775894d39a868a11b64
Karen <karen214765@gmail.com>
2017-10-19 22:58:01 -0600

    Se deja la configuracion para el esquema de seguridad nadamas

---
+++ iDoctores.Library/DataAccess/Entities/Context/SeguridadContext.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Data.Entity;
+using System.Data.Entity.DbContext;
+using iDoctores.DataLayer.Models.Views.QA;
+
+namespace iDoctores.Library.DataAccess.Entities.Context
+
+{
+	public class SeguridadDatabaseModel : DbContext
+	{
+		public SeguridadDatabaseModel() : base("name=DB_A0FD5F_idoctors")
+		{
+
+		}
+
+        public virtual DbSet<Usuario> Usuarios { get; set; }
+        public virtual DbSet<Perfil> Perfiles { get; set; }
+        public virtual DbSet<Permiso> Permisos { get; set; }
+
+        //protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        //{
+          //  modelBuilder.Entity<UsuarioEntity>()
+        //}
+	}
+}
---
+++ iDoctores.Library/DataAccess/Entities/Seguridad/UsuarioEntity.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using iDoctores.Library.DataAccess.Entities;
+
+namespace iDoctores.Library.DataAccess.Entities.Seguridad
+{
+    [Table("usuarios", Schema = "seguridad")]
+    public class UsuarioEntity
+    {
+        [Key]
+        [Column("usuarioid")]
+        public int UsuarioId { get; set; }
+
+		[Column("usuario")]
+        public string Usuario { get; set; }
+
+		[Column("email")]
+		public string Email { get; set; }
+
+		[Column("password")]
+		public string Password { get; set; }
+
+		[Column("fecha_registro")]
+		public string FechaRegistro { get; set; }
+    }
+}
